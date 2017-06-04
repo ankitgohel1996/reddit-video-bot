b44749f0e33cd23afbcb39656bd1eb067252bf4d
Ankit Gohel <ankitgohel1996@gmail.com>
2017-06-04 14:31:28 +0530

    Added opt out feature

--- __init__.py
+++ __init__.py
@@ -18,49 +18,82 @@ def main():
 	
 	subreddit = reddit.subreddit('test')
 	for comment in subreddit.stream.comments():
-		find_comments(comment)		
-	
-def find_comments(comment):
+		process_comments(comment)	
+
+def blacklist_users(comment):
+	comment_text = comment.body.lower()
+
+	if comment_text == 'stop':
+		parent = comment.parent()
+
+		if parent.author != 'video_descriptionbot':
+			return
+
+		with connection.cursor() as cursor:
+			cursor.execute('INSERT INTO blacklist VALUES("%s")' % (comment.author))
+			connection.commit()
+
+def process_comments(comment):
+	print(comment.body)
+	blacklist_users(comment)
+
 	if comment.author == 'video_descriptionbot':
+		return
+
+	if 'bot' in str(comment.author):
 		return
 
 	if comment.score < 1:
 		return
 
-	with connection.cursor() as cursor:
-		cursor.execute('SELECT * FROM  comments WHERE comment_id = "%s"' % (comment.id))
-		result = cursor.rowcount
+	if 'youtube.com' in comment.body or 'youtu.be' in comment.body:
+		with connection.cursor() as cursor:
+			cursor.execute('SELECT * FROM  blacklist WHERE author = "%s"' % (comment.author))
+			result_blacklist = cursor.rowcount
 
-		if result == 0:
+		if result_blacklist == 1:
+			print('User has opted out')
+			return
 
-				if 'youtube.com' in comment.body or 'youtu.be' in comment.body:
-					reply_post = 'SECTION | CONTENT' +'\n' + ':--|:--' +'\n'
+		with connection.cursor() as cursor:
+			cursor.execute('SELECT * FROM  comments WHERE comment_id = "%s"' % (comment.id))
+			result = cursor.rowcount
 
-					if '](' in comment.body:
-						comment_parts = comment.body.split(']')
-						youtube_link = comment_parts[1].split(')')[0][1:]
-						if 'youtube.com' in youtube_link or 'youtu.be' in youtube_link:
-							reply_title,reply_description,reply_duration = find_id(youtube_link)
-							reply_post += 'Title | '+ reply_title +'\n'
-							reply_post += 'Description | '+ reply_description +'\n'
-							reply_post += 'Length | '+ str(reply_duration) +'\n'
-					else:
-						words = comment.body.split()
-						for word in words:
-							if 'youtube.com' in word or 'youtu.be' in word:
-								reply_title,reply_description,reply_duration = find_id(word)
-								reply_post += 'Title | '+ reply_title +'\n'
-								reply_post += 'Description | '+ reply_description +'\n'
-								reply_post += 'Length | '+ str(reply_duration) +'\n'
-					reply_post += '\n'+'*I am a bot, this is an auto-generated reply*'
+			if result == 1:
+				return
 
-					try:
-						comment.reply(reply_post)
-						with connection.cursor() as cursor:
-							cursor.execute('INSERT INTO comments VALUES("%s")' % (comment.id))
-							connection.commit()
-					except Exception as e:
-						print (e)
+			reply_post = 'SECTION | CONTENT' +'\n' + ':--|:--' +'\n'
+
+			if '](' in comment.body:
+				comment_parts = comment.body.split(']')
+				youtube_link = comment_parts[1].split(')')[0][1:]
+				if 'youtube.com' in youtube_link or 'youtu.be' in youtube_link:
+					reply_title,reply_description,reply_duration = find_id(youtube_link)
+					reply_post += 'Title | '+ reply_title +'\n'
+					reply_post += 'Description | '+ reply_description +'\n'
+					reply_post += 'Length | '+ str(reply_duration) +'\n'
+			else:
+				words = comment.body.split()
+				for word in words:
+					if 'youtube.com' in word or 'youtu.be' in word:
+						reply_title,reply_description,reply_duration = find_id(word)
+						reply_post += 'Title | '+ reply_title +'\n'
+						reply_post += 'Description | '+ reply_description +'\n'
+						reply_post += 'Length | '+ str(reply_duration) +'\n'
+
+			reply_post += '\n \n \n \n'+ '****' + '\n \n' + '^(I am a bot, this is an auto-generated reply | )'
+			reply_post += '^[Info](https://www.reddit.com/u/video_descriptionbot) ^| '
+			reply_post += '^[Feedback](https://www.reddit.com/message/compose/?to=video_descriptionbot&subject=Feedback) ^| '
+			reply_post += '^(Reply STOP to opt out permanently)'
+
+			try:
+				comment.reply(reply_post)
+				print('Successfully replied')
+				with connection.cursor() as cursor:
+					cursor.execute('INSERT INTO comments VALUES("%s")' % (comment.id))
+					connection.commit()
+			except Exception as e:
+				print (e)
 	
 def find_id(link):
 	if 'v=' in link:
