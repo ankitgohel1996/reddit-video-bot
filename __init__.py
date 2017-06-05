0e6d3fff1b6f91d28b4cfb37aa1713390d7f31f4
Ankit Gohel <ankitgohel1996@gmail.com>
2017-06-05 14:57:05 +0530

    v1.1: Multiple videos in one comment

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ youtube = youtubeobject.get_authenticated_service(args)
 
 def main():
 	reddit = reddit_auth()
-	subreddit = reddit.subreddit('all')
+	subreddit = reddit.subreddit('test')
 	for comment in subreddit.stream.comments():
 		process_comments(comment)	
 
@@ -55,42 +55,45 @@ def process_comments(comment):
 			cursor.execute('SELECT * FROM  comments WHERE comment_id = "%s"' % (comment.id))
 			result = cursor.rowcount
 
-			if result == 1:
-				return
+		if result == 1:
+			return
 
-			reply_post = 'SECTION | CONTENT' +'\n' + ':--|:--' +'\n'
+		words = comment.body.split()
+		reply_post = ""
 
-			if '](' in comment.body:
-				comment_parts = comment.body.split(']')
-				youtube_link = comment_parts[1].split(')')[0][1:]
-				if 'youtube.com' in youtube_link or 'youtu.be' in youtube_link:
-					reply_title,reply_description,reply_duration = find_id(youtube_link)
-					reply_post += 'Title | '+ reply_title +'\n'
-					reply_post += 'Description | '+ reply_description +'\n'
-					reply_post += 'Length | '+ str(reply_duration) +'\n'
-			else:
-				words = comment.body.split()
-				for word in words:
-					if 'youtube.com' in word or 'youtu.be' in word:
-						reply_title,reply_description,reply_duration = find_id(word)
-						reply_post += 'Title | '+ reply_title +'\n'
-						reply_post += 'Description | '+ reply_description +'\n'
-						reply_post += 'Length | '+ str(reply_duration) +'\n'
+		for word in words:
+			if 'youtube.com' in word or 'youtu.be' in word:
+				if '](' in word:
+					comment_parts = word.split(']')
+					youtube_link = comment_parts[1].split(')')[0][1:]
+					reply_post += create_reply(youtube_link)
+				else:
+					reply_post += create_reply(word)
 
-			reply_post += '\n \n \n \n'+ '****' + '\n \n' + '^(I am a bot, this is an auto-generated reply | )'
-			reply_post += '^[Info](https://www.reddit.com/u/video_descriptionbot) ^| '
-			reply_post += '^[Feedback](https://www.reddit.com/message/compose/?to=video_descriptionbot&subject=Feedback) ^| '
-			reply_post += '^(Reply STOP to opt out permanently)'
+		reply_post += '\n \n \n \n'+ '****' + '\n \n' + '^(I am a bot, this is an auto-generated reply | )'
+		reply_post += '^[Info](https://www.reddit.com/u/video_descriptionbot) ^| '
+		reply_post += '^[Feedback](https://www.reddit.com/message/compose/?to=video_descriptionbot&subject=Feedback) ^| '
+		reply_post += '^(Reply STOP to opt out permanently)'
 
-			try:
-				comment.reply(reply_post)
-				print('Successfully replied')
-				with connection.cursor() as cursor:
-					cursor.execute('INSERT INTO comments VALUES("%s")' % (comment.id))
-					connection.commit()
-			except Exception as e:
-				print (e)
-	
+		try:
+			comment.reply(reply_post)
+			print('Successfully replied')
+			with connection.cursor() as cursor:
+				cursor.execute('INSERT INTO comments VALUES("%s")' % (comment.id))
+				connection.commit()
+		except Exception as e:
+			print (e)
+
+def create_reply(body):
+	reply = 'SECTION | CONTENT' +'\n' + ':--|:--' +'\n'
+	reply_title,reply_description,reply_duration = find_id(body)
+	reply += 'Title | '+ reply_title +'\n'
+	reply += 'Description | '+ reply_description +'\n'
+	reply += 'Length | '+ str(reply_duration) +'\n'
+	reply += '\n \n'
+
+	return reply
+
 def find_id(link):
 	if 'v=' in link:
 		link_parts = link.split('v=')
